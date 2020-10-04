48e89164eb51ed94bf57c19b443fcbd996881408
Sazonov Nikita <actpohabt.ns@gmail.com>
2020-10-04 16:49:10 +0300

    [ REFACTORED ] Marked some functions const

--- lab01/src/Graph/Graph.h
+++ lab01/src/Graph/Graph.h
@@ -9,20 +9,20 @@ public:
 
     virtual void addNode(const NT& data) = 0;
 //    virtual void eraseNode(const NT& data) = 0;
-    virtual bool nodeExist(const NT& data)const = 0;
+    virtual bool nodeExist(const NT& data) const = 0;
 
     virtual void addEdge(const NT& n1, const NT& n2, const ET& edgeData) = 0;
 //    virtual void eraseEdge(const NT& n1, const NT& n2) = 0;
-//    virtual bool edgeExist(const ET& edgeData) = 0;
-//    virtual bool edgeExist(const NT& n1, const NT& n2) = 0;
+//    virtual bool edgeExist(const ET& edgeData) const = 0;
+    virtual bool edgeExist(const NT& n1, const NT& n2) const = 0;
 //    virtual void eraseEdges() = 0;
 
 //    virtual void weighed(bool weighed) = 0;
 
-//    virtual bool connected() = 0;
-//    virtual bool cyclic() = 0;
+//    virtual bool connected() const = 0;
+//    virtual bool cyclic() const = 0;
 
-//    [[nodiscard]] virtual int distance(const NT& n1, const NT& n2) = 0;
+//    [[nodiscard]] virtual int distance(const NT& n1, const NT& n2) const = 0;
 
     virtual int nodes() const = 0;
     virtual int edges() const = 0;
