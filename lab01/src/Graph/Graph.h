745935c85de861afbd098d3c568e6e6c89ab91d4
Sazonov Nikita <actpohabt.ns@gmail.com>
2020-10-04 18:03:42 +0300

    [ REFACTORED ] bool edgeExist(const ET& edgeData) replaced with const ET* getEdge

--- lab01/src/Graph/Graph.h
+++ lab01/src/Graph/Graph.h
@@ -13,7 +13,7 @@ public:
 
     virtual void addEdge(const NT& n1, const NT& n2, const ET& edgeData) = 0;
 //    virtual void eraseEdge(const NT& n1, const NT& n2) = 0;
-//    virtual bool edgeExist(const ET& edgeData) const = 0;
+    virtual const ET* getEdge(const NT& n1, const NT& n2) const = 0;
     virtual bool edgeExist(const NT& n1, const NT& n2) const = 0;
 //    virtual void eraseEdges() = 0;
 
