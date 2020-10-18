800112e4ee4fdc139bb87764efa4a0e4b63884b9
Sazonov Nikita <actpohabt.ns@gmail.com>
2020-10-18 23:16:47 +0300

    [ ADDED ] members initialization, createNewGraphButton clicked

--- lab01/src/UI/mainwindow.cpp
+++ lab01/src/UI/mainwindow.cpp
@@ -6,6 +6,9 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    this->_console = new Console(ui->graphConsole);
+    this->_initGraphs();
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +16,30 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::_initGraphs()
+{
+    this->_lgr = new LGraph<Network, int>();
+    this->_mgr = new MGraph<Network, int>();
+
+    this->_currGraph = dynamic_cast<Graph<Network, int>*>(this->_lgr);
+    this->_curr_IA_T = IA_t::IPv4;
+}
+
+
+void MainWindow::on_createNewGraphButton_clicked()
+{
+    switch (this->_curr_IA_T) {
+        case IA_t::IPv4:
+            this->_lgr = new LGraph<Network, int>();
+            break;
+
+        case IA_t::IPv6:
+            this->_mgr = new MGraph<Network, int>();
+            break;
+    }
+
+    this->_console->newPar();
+    this->_console->printTech(this->_currGraph->typeStr() + ": ");
+    this->_console->newLine();
+    this->_console->print("Created new graph!");
+}
--- lab01/src/UI/mainwindow.h
+++ lab01/src/UI/mainwindow.h
@@ -1,6 +1,14 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include "../Graph/Graph.h"
+#include "../Graph/LGraph.h"
+#include "../Graph/MGraph.h"
+
+#include "../Networks/network.h"
+
+#include "console.h"
+
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
@@ -15,7 +23,21 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_createNewGraphButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+
+    MGraph<Network, int>* _mgr;
+    LGraph<Network, int>* _lgr;
+
+    Graph<Network, int>* _currGraph;
+
+    Console* _console;
+
+    IA_t _curr_IA_T;
+
+    void _initGraphs();
 };
 #endif // MAINWINDOW_H
