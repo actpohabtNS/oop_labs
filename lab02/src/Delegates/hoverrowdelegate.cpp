578a4d51ca6f2197535489b452861cfd528b91f1
Sazonov Nikita <actpohabt.ns@gmail.com>
2020-11-23 10:40:36 +0200

    [ RAFACTORED ] added hoverRow, hoverColor

--- lab02/src/Delegates/hoverrowdelegate.cpp
+++ lab02/src/Delegates/hoverrowdelegate.cpp
@@ -3,14 +3,14 @@
 #include <QPainter>
 
 HoverRowDelegate::HoverRowDelegate(QObject *parent)
-    : QStyledItemDelegate{parent}, _hovered_row{-1} {}
+    : QStyledItemDelegate{parent}, _hoveredRow{-1}, _hoveredColumn{-1}, _hoverColor("#4f4f50") {}
 
 void HoverRowDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const
 {
     QStyleOptionViewItem opt = option;
-    if(index.row() == _hovered_row) {
+    if(index.row() == _hoveredRow) {
         opt.state |= QStyle::State_MouseOver;
-        painter->fillRect(option.rect, QBrush{"#4f4f50"});
+        painter->fillRect(option.rect, QBrush{_hoverColor});
     } else {
         opt.state &= ~QStyle::State_MouseOver;
     }
@@ -18,12 +18,19 @@ void HoverRowDelegate::paint(QPainter *painter, const QStyleOptionViewItem &opti
     QStyledItemDelegate::paint(painter, opt, index);
 }
 
+void HoverRowDelegate::setHoverColor(const QColor &color)
+{
+    _hoverColor = color;
+}
+
 void HoverRowDelegate::onHoverIndexChanged(const QModelIndex &item)
 {
-    _hovered_row = item.row();
+    _hoveredRow = item.row();
+    _hoveredColumn = item.column();
 }
 
 void HoverRowDelegate::onLeaveTableEvent()
 {
-    _hovered_row = -1;
+    _hoveredRow = -1;
+    _hoveredColumn = -1;
 }
--- lab02/src/Delegates/hoverrowdelegate.h
+++ lab02/src/Delegates/hoverrowdelegate.h
@@ -2,18 +2,23 @@
 #define HOVERROWDELEGATE_H
 
 #include <QStyledItemDelegate>
+#include <QColor>
 
 class HoverRowDelegate : public QStyledItemDelegate
 {
     Q_OBJECT
 
 protected:
-    int _hovered_row;
+    int _hoveredRow;
+    int _hoveredColumn;
+    QColor _hoverColor;
 
 public:
     HoverRowDelegate(QObject *parent = nullptr);
 
     void paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const;
+
+    void setHoverColor(const QColor& color);
 
 public slots:
     void onHoverIndexChanged(const QModelIndex& item);
