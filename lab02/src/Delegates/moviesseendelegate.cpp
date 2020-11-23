6ee5a1a1f68c8638705b917f7fe28e4da603b6cc
Sazonov Nikita <actpohabt.ns@gmail.com>
2020-11-23 11:52:47 +0200

    [ DONE ] draw copy and delete icons on hover

--- lab02/src/Delegates/moviesseendelegate.cpp
+++ lab02/src/Delegates/moviesseendelegate.cpp
@@ -1,6 +1,7 @@
 #include "moviesseendelegate.h"
 
 #include <QApplication>
+#include <QPainter>
 
 MoviesSeenDelegate::MoviesSeenDelegate(QObject *parent)
     : HoverRowDelegate(parent) {}
@@ -9,17 +10,43 @@ void MoviesSeenDelegate::paint(QPainter *painter, const QStyleOptionViewItem &op
 {
     HoverRowDelegate::paint(painter, option, index);
 
-    if (index.row() == _hoveredRow && index.column() == 0) {
-        QStyleOptionButton button;
-             QRect r = option.rect;//getting the rect of the cell
-             int x,y,w,h;
-             w = 35;
-             h = 35;
-             x = r.left() + r.width() - w/2;//the X coordinate
-             y = r.top() + r.height()/2 - h/2;//the Y coordinate
-             button.rect = QRect(x,y,w,h);
-             button.text = "copy";//no text . since if text will be given then it will push the icon to left side based on the coordinates .
-             button.state = QStyle::State_Enabled;
-             QApplication::style()->drawControl( QStyle::CE_PushButtonLabel, &button, painter);
+    if (index.row() == _hoveredRow) {
+        QRect r = option.rect;//getting the rect of the cell
+        int x,y, fontSize;
+        fontSize = 18;
+        x = r.left() + r.width()/2 - fontSize/2;//the X coordinate
+        y = r.top() + r.height()/2 + fontSize/2;//the Y coordinate
+
+        painter->setFont({"Roboto", fontSize});
+
+        if (index.column() == 0) {
+            painter->setPen("#7a7d7d");
+
+            if (_hoveredColumn == 0) {
+                 QApplication::setOverrideCursor(QCursor(Qt::PointingHandCursor));
+
+                 painter->setPen("#b3b3b3");
+            }
+
+            painter->drawText(x, y, "⎘");
+        }
+
+        if (index.column() == 7) {
+            painter->setPen("#7a7d7d");
+
+            if (_hoveredColumn == 7) {
+                 QApplication::setOverrideCursor(QCursor(Qt::PointingHandCursor));
+
+                 painter->setPen("#ff0000");
+            }
+
+            painter->drawText(x, y, "×");
+        }
+
+        QStyledItemDelegate::paint(painter, option, index);
     }
+
+    if (_hoveredColumn != 0 && _hoveredColumn != 7)
+        QApplication::restoreOverrideCursor();
+
 }
